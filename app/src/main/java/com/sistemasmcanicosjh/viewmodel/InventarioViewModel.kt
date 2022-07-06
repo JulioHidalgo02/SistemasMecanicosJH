d2afc25c17c95cf90d8d41762539af5381a95e5e
JulioHidalgo02 <j.hidalgoro@hotmail.com>
2022-07-06 17:10:41 -0600

    Implementación y mejoras de diseño de la aplicación, ademas de funcionamiento del login.

---
+++ app/src/main/java/com/sistemasmcanicosjh/viewmodel/InventarioViewModel.kt
@@ -0,0 +1,13 @@
+package com.sistemasmcanicosjh.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class InventarioViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
